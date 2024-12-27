6883eb2d2cf67caa08f71b84b3f14145017baa29
Om Patel <18bce165@nirmauni.ac.in>
2024-12-27 13:33:51 -0700

    Managing Application State with Redux.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode:'development',
@@ -12,8 +13,15 @@ module.exports = {
         extensions: ['.js', '.jsx']
     },
     devServer: {
+        static: path.resolve(__dirname, 'dist'),
         historyApiFallback: true
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'index.html'),
+            filename: 'index.html'
+        })
+    ],
     module: {
         rules: [{
             test: /\.jsx?/,
