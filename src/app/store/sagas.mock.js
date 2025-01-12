68a5763939e95f5ff079d8f8738e11024ac8ff41
Om Patel <18bce165@nirmauni.ac.in>
2025-01-11 20:47:06 -0700

    Implementing Task Details Route Part 1 - Mutating Data

--- src/app/store/sagas.mock.js
+++ src/app/store/sagas.mock.js
@@ -5,7 +5,7 @@ import { v4 as uuid } from 'uuid';
 export function* taskCreationSaga(){
     while (true){
         const {groupID} = yield take(mutations.REQUEST_TASK_CREATION);
-        const ownerID = yield select(state=>state.session.id);
+        const ownerID = yield select(state=>state.session);
         const taskID = uuid();
         yield put(mutations.createTask(taskID, groupID, ownerID));
         
