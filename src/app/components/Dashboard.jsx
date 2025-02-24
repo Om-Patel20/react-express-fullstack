f4cc9ded7bebcdd7fb42e06bc82096ed38ec4253
Om Patel <18bce165@nirmauni.ac.in>
2025-02-23 18:57:19 -0700

    Adding Styling and Presentational Elements.

--- src/app/components/Dashboard.jsx
+++ src/app/components/Dashboard.jsx
@@ -3,10 +3,11 @@ import { connect } from "react-redux";
 import { ConnectedTaskList } from "./TaskList";
 
 export const Dashboard = ({groups})=>(
-    <div>
-        <h2>Dashboard</h2>
-        {groups.map(group=>(
-            <ConnectedTaskList key={group.id} id={group.id} name={group.name}/>
+    <div className="row">
+            {groups.map(group => (
+            <div key={group.id} className="col">
+                <ConnectedTaskList id={group.id} name={group.name} />
+            </div>
         ))}
     </div>
 );
